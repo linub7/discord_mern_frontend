bb057a39dd7b5d92cd173730bebd4952839376c1
a1a2a3a4a5 <linub7@gmail.com>
2022-09-16 21:38:57 +0430

    fix silly error

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import Cookies from 'js-cookie';
 import { logout } from 'utils/auth';
 
 const apiClient = axios.create({
-  baseURL: 'REACT_APP_BACKEND_URL/api/v1',
+  baseURL: `${process.env.REACT_APP_BACKEND_URL}/api/v1`,
   // baseURL: 'http://localhost:8000/api/v1',
 });
 
